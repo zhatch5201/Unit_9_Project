b8b02bf2bcc805a1c85c8d8920acc713fb944aa1
zhatch5201 <zhatch164@wesst-mec.org>
2019-12-06 14:27:39 -0700

    Done

--- case2/sb_script.js
+++ case2/sb_script.js
@@ -23,11 +23,8 @@
 window.onload = init;
 
 function init() {
-   document.forms[0].onsubmit = function() {
-      if (this.checkValidity()) alert("Data passes initial validation tests");
-      return false;
-   }
-   
+	document.forms[0].onsubmit = function() {
+		if (this.checkValidity()) alert('Data passes initial validation tests');
+		return false;
+	};
 }
-
-  
